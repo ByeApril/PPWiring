be351d9aa7b780b13c1034949c6e5402663df998
ByeApril <st1merus@gmail.com>
2024-03-08 21:56:42 +0300

    1

--- src/main/java/koschei/models/Wood3.java
+++ src/main/java/koschei/models/Wood3.java
@@ -6,9 +6,10 @@ import org.springframework.stereotype.Component;
 @Component
 public class Wood3 {
 
-    private final Rabbit4 rabbit;
-
     @Autowired
+    private Rabbit4 rabbit;
+
+
     public Wood3(Rabbit4 rabbit) {
         this.rabbit = rabbit;
     }
